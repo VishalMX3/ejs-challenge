a9ddb493e858288e3dc6dd8763f88ca3b66df2e8
VishalMX3 <f20191055@pilani.bits-pilani.ac.in>
2024-01-15 15:54:46 +0530

    Changed the home content

--- app.js
+++ app.js
@@ -1,22 +1,26 @@
-
-
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
-const _ = require('lodash');
-const mongoose = require("mongoose"); 
+const _ = require("lodash");
+const mongoose = require("mongoose");
 
 const PORT = process.env.PORT | 5000;
 
-const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
-const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
-const contactContent = "Scelerisque eleifend donec pretium vulputate sapien. Rhoncus urna neque viverra justo nec ultrices. Arcu dui vivamus arcu felis bibendum. Consectetur adipiscing elit duis tristique. Risus viverra adipiscing at in tellus integer feugiat. Sapien nec sagittis aliquam malesuada bibendum arcu vitae. Consequat interdum varius sit amet mattis. Iaculis nunc sed augue lacus. Interdum posuere lorem ipsum dolor sit amet consectetur adipiscing elit. Pulvinar elementum integer enim neque. Ultrices gravida dictum fusce ut placerat orci nulla. Mauris in aliquam sem fringilla ut morbi tincidunt. Tortor posuere ac ut consequat semper viverra nam libero.";
+const homeStartingContent =
+  "Hi, I'm Vishal.🙋‍♂️ I'm a blend of tech and creativity, casting a spell where my technical skills seamlessly merge with creative flair to elevate user experiences. Every piece of code transforms into a creative touch, maximizing the magic in the digital world";
+const aboutContent =
+  "This is a Blog Journal Website where one can post a blog post. Compose your blog and let us now your thoughts. ";
+const contactContent =
+  "Hey, we can connect through email : vishalpundhirofficial@gmail.com";
 
-mongoose.connect("mongodb+srv://admin-vishal:Ronaldo-73@cluster0.jtdoo3s.mongodb.net/blogDB", {useNewUrlParser : true});
+mongoose.connect(
+  "mongodb+srv://admin-vishal:Ronaldo-73@cluster0.jtdoo3s.mongodb.net/blogDB",
+  { useNewUrlParser: true }
+);
 
 const postSchema = {
   title: String,
-  content: String
+  content: String,
 };
 
 const Post = new mongoose.model("Post", postSchema);
@@ -25,42 +29,48 @@ let posts = [];
 
 const app = express();
 
-app.set('view engine', 'ejs');
+app.set("view engine", "ejs");
 
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
-app.get("/", function(req, res){
-  Post.find({}).then(function(foundPosts){
-    res.render("home", {startingContent: homeStartingContent, postArray: foundPosts});
-  }).catch(function(err){
-    console.log(err);
-  });
+app.get("/", function (req, res) {
+  Post.find({})
+    .then(function (foundPosts) {
+      res.render("home", {
+        startingContent: homeStartingContent,
+        postArray: foundPosts,
+      });
+    })
+    .catch(function (err) {
+      console.log(err);
+    });
 });
 
-app.get("/about", function(req, res){
-  res.render("about", {content: aboutContent});
+app.get("/about", function (req, res) {
+  res.render("about", { content: aboutContent });
 });
 
-app.get("/contact", function(req, res){
-  res.render("contact", {content: contactContent });
+app.get("/contact", function (req, res) {
+  res.render("contact", { content: contactContent });
 });
 
-app.get("/compose", function(req, res){
+app.get("/compose", function (req, res) {
   res.render("compose");
 });
 
-app.get("/post/:postID", function(req, res){
+app.get("/post/:postID", function (req, res) {
   const requestedPostID = req.params.postID;
-  Post.findOne({_id: requestedPostID}).then(function(post){
-
-    res.render("post", {
-      title: post.title,
-      content: post.content
- });
-}).catch(function(err){
-    console.log(error);
-  });
+  Post.findOne({ _id: requestedPostID })
+    .then(function (post) {
+      res.render("post", {
+        title: post.title,
+        content: post.content,
+      });
+    })
+    .catch(function (err) {
+      console.log(error);
+    });
 
   // let requiredContent = "";
   // posts.forEach(post => {
@@ -71,42 +81,29 @@ app.get("/post/:postID", function(req, res){
   // res.render("post", {topic: _.lowerCase(req.params.postID), content: requiredContent });
 });
 
-app.post("/compose", function(req, res){
+app.post("/compose", function (req, res) {
   const post = new Post({
     title: req.body.postTitle,
-    content: req.body.postContent
+    content: req.body.postContent,
   });
 
-  post.save().then(function(){
-    res.redirect("/");
-  }).catch(function(err){
-    console.log(err);
-  });
+  post
+    .save()
+    .then(function () {
+      res.redirect("/");
+    })
+    .catch(function (err) {
+      console.log(err);
+    });
 
-  // const post = { 
+  // const post = {
   //   title: req.body.postTitle,
   //   content: req.body.postContent
   // };
 
   // posts.push(post);
-  
-
-
 });
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-app.listen(PORT, function() {
+app.listen(PORT, function () {
   console.log("Server started on port 5000");
 });
